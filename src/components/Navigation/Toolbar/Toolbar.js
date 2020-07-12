4a6b2aa0ef51fdd38e736b304735dbcbb1ec6214
nicky-t <nickytaketo0903@gmail.com>
2020-07-12 22:22:55 +0900

    add: headerを追加

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import classes from "./Toolbar.module.css";
+
+const toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>menu</div>
+    <div>logo</div>
+    <nav>
+      <ul>...</ul>
+    </nav>
+  </header>
+);
+
+export default toolbar;
