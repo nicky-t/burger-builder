3a885ccdf1cf1be9280bd1301bafc2a8c089e873
nicky-t <nickytaketo0903@gmail.com>
2020-07-15 17:24:14 +0900

    add: httpのエラー処理の実装

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -10,7 +10,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
     };
 
     componentWillMount() {
-      axios.interceptors.response.use((req) => {
+      this.reqInterceptor = axios.interceptors.response.use((req) => {
         this.setState({ error: null });
         return req;
       });
@@ -20,6 +20,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.reqInterceptor);
     }
 
     errorConfirmedHandler = () => {
